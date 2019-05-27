075aacb9fb470801e37a8bc69ba2cd5a992dcb53
Eddy Ekofo <eddy.ekofo@amadeus.com>
2019-05-27 13:08:18 +0200

    Add script to run code using cmake

--- run_code.sh
+++ run_code.sh
@@ -1,1 +1,17 @@
-cmake ../ && make && ./src/main
+#!/bin/bash
+
+PROJECT_ROOT_DIR="${0%/*}"
+BUILD_DIR="$PROJECT_ROOT_DIR/build"
+WORKING_DIR=$(pwd)
+
+if [[ $(pwd) != $PROJECT_ROOT_DIR ]]; then
+    cd $PROJECT_ROOT_DIR
+    if [ ! -d "$BUILD_DIR" ]; then
+        mkdir $BUILD_DIR
+        cd $BUILD_DIR
+        cmake ../ && make && ./src/main
+    else
+        cd $BUILD_DIR
+        cmake ../ && make && ./src/main      
+    fi
+fi
